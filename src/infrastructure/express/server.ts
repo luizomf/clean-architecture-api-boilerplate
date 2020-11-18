5fc4d9eeaf260467b45f087dce0a11b4362a9772
Luiz Otavio <luizomf@gmail.com>
2020-11-18 16:54:24 -0300

    removed getPort function

--- src/infrastructure/express/server.ts
+++ src/infrastructure/express/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { setupAsyncErrors } from './setup/setup-async-errors';
 import { setupGlobalMiddlewares } from './setup/setup-global-middlewares';
 import { setupRoutes } from './setup/setup-routes';
-import { getPort } from './utils/get-port';
 
 export const app = express();
 
@@ -10,7 +9,9 @@ setupGlobalMiddlewares(app);
 setupRoutes(app);
 setupAsyncErrors(app); // It has to be placed after all routes and middlewares
 
-const port = getPort();
-app.listen(port, () => {
-  console.log(`Server listening -> http://127.0.0.1:${port}`);
-});
+const port = process.env.PORT || 4321;
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Server listening -> http://127.0.0.1:${port}`);
+  });
+}
--- src/infrastructure/express/utils/get-port.ts
+++
@@ -1,6 +0,0 @@
-export const getPort = () => {
-  if (process.env.NODE_ENV === 'test') {
-    return 4322;
-  }
-  return process.env.PORT || 4321;
-};
