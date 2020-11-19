ade2032f882f649a5e34ff89f6014f7bdcaa3288
Luiz Otavio <luizomf@gmail.com>
2020-11-19 13:42:52 -0300

    remove some path from coverage and test

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,16 @@ module.exports = {
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
   },
+  testPathIgnorePatterns: [
+    '<rootDir>/build/',
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/infrastructure/knex/migrations',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/migrations/*.ts',
+    '!src/**/console-application/*.ts',
+    '!src/**/middlewares/example.ts',
+  ],
 };
