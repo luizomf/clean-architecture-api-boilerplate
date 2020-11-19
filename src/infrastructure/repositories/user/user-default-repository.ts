7e7488044c5d0f04d65a1a3ee4133e955ffac0b9
Luiz Otavio <luizomf@gmail.com>
2020-11-19 13:45:49 -0300

    fix user default repository with correct repositories

--- src/infrastructure/repositories/user/user-default-repository.ts
+++ src/infrastructure/repositories/user/user-default-repository.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { CreateUserRepository } from '~/application/ports/repositories/create-user-repository';
+import { FindUserByEmailRepository } from '~/application/ports/repositories/find-user-by-email-repository';
+import { FindUserByIdRepository } from '~/application/ports/repositories/find-user-by-id-repository';
 import { UserSqlRepository } from './sql/user-sql-repository';
 import { inMemoryUserRepositoryFactory } from './testing-repository/in-memory-user-repository-factory';
 
@@ -6,9 +9,9 @@ import { inMemoryUserRepositoryFactory } from './testing-repository/in-memory-us
 const userSqlRepository = new UserSqlRepository();
 const inMemoryUserRepository = inMemoryUserRepositoryFactory();
 
-const createUserRepository = userSqlRepository;
-const findUserByIdRepository = userSqlRepository;
-const findUserByEmailRepository = userSqlRepository;
+const createUserRepository: CreateUserRepository = userSqlRepository;
+const findUserByIdRepository: FindUserByIdRepository = userSqlRepository;
+const findUserByEmailRepository: FindUserByEmailRepository = userSqlRepository;
 
 export {
   createUserRepository,
