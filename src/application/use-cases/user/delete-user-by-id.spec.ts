b6e77e54ee9e90ba607a888edbfd27e8083eeb93
Luiz Otavio <luizomf@gmail.com>
2020-11-23 21:23:43 -0300

    add DeleteUserById use case

--- src/application/use-cases/user/delete-user-by-id.spec.ts
+++ src/application/use-cases/user/delete-user-by-id.spec.ts
@@ -1,7 +1,6 @@
 import { DeleteUserByIdRepository } from '~/application/ports/repositories/delete-user-by-id-repository';
 import { FindUserByIdRepository } from '~/application/ports/repositories/find-user-by-id-repository';
 import { User } from '~/domain/user/user';
-import { db } from '~/infrastructure/knex/connection';
 import { DeleteUserById } from './delete-user-by-id';
 
 const sutFactory = () => {
@@ -52,18 +51,26 @@ const deleteUserByIdRepositoryMockFactory = () => {
 };
 
 describe('DeleteUserById', () => {
-  beforeAll(async () => {
-    await db.migrate.latest({ directory: process.env.MIGRATIONS });
-    await db<User>('users').insert({
-      first_name: 'first',
-      last_name: 'last',
-      email: 'email@email.com',
-      password_hash: 'any_hash',
-    });
+  it('should call findUserByIdRepository with correct values', async () => {
+    const { sut, findUserByIdRepositoryMock } = sutFactory();
+    const findUserByIdRepositorySpy = jest.spyOn(
+      findUserByIdRepositoryMock,
+      'findById',
+    );
+    await sut.deleteById('20');
+    expect(findUserByIdRepositorySpy).toHaveBeenCalledTimes(1);
+    expect(findUserByIdRepositorySpy).toHaveBeenCalledWith('20');
   });
 
-  afterAll(async () => {
-    await db.destroy();
+  it('should call deleteUserByIdRepository with correct values', async () => {
+    const { sut, deleteUserByIdRepositoryMock } = sutFactory();
+    const deleteUserByIdRepositorySpy = jest.spyOn(
+      deleteUserByIdRepositoryMock,
+      'deleteById',
+    );
+    await sut.deleteById('20');
+    expect(deleteUserByIdRepositorySpy).toHaveBeenCalledTimes(1);
+    expect(deleteUserByIdRepositorySpy).toHaveBeenCalledWith('20');
   });
 
   it('should delete a user if exists', async () => {
@@ -86,5 +93,22 @@ describe('DeleteUserById', () => {
     }
 
     expect(error.statusCode).toBe(404);
+  });
+
+  it('should throw deleteUserByIdRepository throws', async () => {
+    const { sut, deleteUserByIdRepositoryMock } = sutFactory();
+    jest
+      .spyOn(deleteUserByIdRepositoryMock, 'deleteById')
+      .mockRejectedValue(new Error());
+
+    let error;
+
+    try {
+      await sut.deleteById('abc');
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error.name).toBe('Error');
   });
 });
