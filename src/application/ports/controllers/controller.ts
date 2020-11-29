23467ed30d2353a671210ff0f5a8eccd3de0566a
Luiz Otavio <luizomf@gmail.com>
2020-11-29 13:04:05 -0300

    refactor: add default type unknown in controller interface

--- src/application/ports/controllers/controller.ts
+++ src/application/ports/controllers/controller.ts
@@ -1,6 +1,6 @@
 import { RequestModel } from '~/application/ports/requests/request-model';
 import { ResponseModel } from '~/application/ports/responses/response-model';
 
-export interface Controller<T> {
+export interface Controller<T = unknown> {
   handleRequest(requestModel: RequestModel): Promise<ResponseModel<T>>;
 }
