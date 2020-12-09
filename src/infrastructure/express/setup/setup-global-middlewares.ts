f4d63b8ab183e9d03f10a20b7c4f6a76d30c094c
Luiz Otavio <luizomf@gmail.com>
2020-12-09 19:37:22 -0300

    feat: add helmet

--- src/infrastructure/express/setup/setup-global-middlewares.ts
+++ src/infrastructure/express/setup/setup-global-middlewares.ts
@@ -1,5 +1,7 @@
 import { Application, json } from 'express';
+import helmet from 'helmet';
 
 export const setupGlobalMiddlewares = (app: Application): void => {
+  app.use(helmet());
   app.use(json());
 };
